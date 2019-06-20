18da88ce01f9ab4c98617413f10af7fc3e2523bc
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2019-06-20 14:46:58 +0200

    test(config): “libvirtd.conf” must use the pillar values
    * test/integration/default/controls/config_spec.rb: parse the
      configuration file and verify the value of each parameters set in
      “pillar.example”.

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -4,6 +4,21 @@
 # Author: Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
 # Copyright © 2019 Pôle de Compétences Logiciels Libres <eole@ac-dijon.fr>
 #
+
+params = {
+  'listen_tls' => '0',
+  'listen_tcp' => '0',
+  'tls_port' => '16514',
+  'tcp_port' => '16509',
+  'listen_addr' => '0.0.0.0',
+  'unix_sock_group' => 'root',
+  'unix_sock_ro_perms' => '0777',
+  'unix_sock_rw_perms' => '0770',
+  'auth_unix_ro' => 'none',
+  'auth_unix_rw' => 'none',
+  'auth_tcp' => 'none',
+}
+
 control 'Libvirt configuration' do
   title 'verify applied configuration'
 
@@ -15,6 +30,17 @@ control 'Libvirt configuration' do
   describe file('/etc/libvirt/libvirtd.conf') do
     it { should exist }
     its('content') { should match %r{This\sfile\sis\smanaged\sby\sSalt} }
+  end
+
+  parse_options = {
+    # Value can be surrounded by quotes, or not
+    assignment_regex: /^\s*([^=]*?)\s*=\s*['"]?(.*?)['"]?\s*$/,
+  }
+
+  describe parse_config_file('/etc/libvirt/libvirtd.conf', parse_options) do
+    params.each do | param, value |
+      its(param) { should eq value }
+    end
   end
 
 end
