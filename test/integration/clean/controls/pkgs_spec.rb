0978ab0e6deb847c1e3e31eb887a22211013080d
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2019-09-02 14:27:07 +0200

    test(clean): check for clean states

---
+++ test/integration/clean/controls/pkgs_spec.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+#
+# pkgs_spec.rb -- Libvirt packages InSpec control
+# Author: Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
+# Copyright © 2019 Pôle de Compétences Logiciels Libres <eole@ac-dijon.fr>
+#
+packages = libvirt.packages
+
+control 'Libvirt packages' do
+  title 'must be uninstalled'
+
+  packages.each do |service, pkgs|
+    pkgs.each do |pkg|
+      describe package(pkg) do
+        it { should_not be_installed }
+      end
+    end
+  end
+
+end
