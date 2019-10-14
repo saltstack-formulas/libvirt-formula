9108afb1004978675e00a41dcee4c10e53c17841
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2019-10-14 15:56:06 +0200

    fix(rubocop): use “.zero?” instead of “== 0”

--- test/integration/share/libraries/salt_minion.rb
+++ test/integration/share/libraries/salt_minion.rb
@@ -46,8 +46,8 @@ class SaltMinionResource < Inspec.resource(1)
   end
 
   def try_python_import_salt
-    return 3 if try_python3_import_salt == 0
-    return 2 if try_python2_import_salt == 0
+    return 3 if try_python3_import_salt.zero?
+    return 2 if try_python2_import_salt.zero?
     raise Inspec::Exceptions::ResourceSkipped, "Unable to import salt from python2 or python3"
   end
 
