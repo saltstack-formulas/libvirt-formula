91c0a44fa0b9df5bacd722fd416764bae5bd5076
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2020-01-07 13:16:32 +0100

    test(inspec): libvirt resource library is too big
    * test/integration/share/libraries/libvirt_packages.rb: dedicated
      resource for libvirt packages.
    
    * test/integration/share/libraries/libvirt.rb: load packages from the
      dedicated resource.

--- test/integration/share/libraries/libvirt.rb
+++ test/integration/share/libraries/libvirt.rb
@@ -16,7 +16,7 @@ class LibvirtResource < Inspec.resource(1)
   attr_reader :packages
 
   def initialize
-    @packages = build_packages
+    @packages = inspec.libvirt_packages.packages
   end
 
   def daemon_config_dir
@@ -33,76 +33,5 @@ class LibvirtResource < Inspec.resource(1)
 
   def daemon_config_file
     inspec.file(File.join(daemon_config_dir, 'libvirtd'))
-  end
-
-  def build_packages
-    # defaults.yaml
-    packages = build_default_packages
-
-    packages.merge!(build_overwrite_packages)
-  end
-
-  def build_overwrite_packages
-    # osfamily.yaml / osmap.yaml
-    case inspec.os[:family]
-    when 'debian'
-      build_debian_packages
-
-    when 'fedora'
-      build_fedora_packages
-
-    when 'suse'
-      build_suse_packages
-
-    else
-      {}
-    end
-  end
-
-  def build_default_packages
-    {
-      'libvirt' => ['libvirt'],
-      'qemu' => ['qemu-kvm'],
-      'extra' => ['libguestfs'],
-      'python' => if inspec.salt_minion.python3?
-                    ['libvirt-python3']
-                  else
-                    ['libvirt-python']
-                  end
-    }
-  end
-
-  def build_debian_packages
-    {
-      'libvirt' => ['libvirt-daemon-system'],
-      'extra' => %w[libguestfs0 libguestfs-tools gnutls-bin virt-top],
-      'python' => if inspec.salt_minion.python3?
-                    ['python3-libvirt']
-                  else
-                    ['python-libvirt']
-                  end
-    }
-  end
-
-  def build_fedora_packages
-    {
-      'python' => if inspec.salt_minion.python3?
-                    ['python3-libvirt']
-                  else
-                    ['python2-libvirt']
-                  end
-    }
-  end
-
-  def build_suse_packages
-    {
-      'libvirt' => ['libvirt-daemon-qemu'],
-      'extra' => ['libguestfs0'],
-      'python' => if inspec.salt_minion.python3?
-                    ['python3-libvirt-python']
-                  else
-                    ['python2-libvirt-python']
-                  end
-    }
   end
 end
