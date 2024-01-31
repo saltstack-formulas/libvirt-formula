03f138b20e832b009c8a88ce4e76d3ca548f38a3
Didier METRAL <didier.metral@fraudbuster.mobi>
2024-01-31 16:47:20 +0100

    fix(onedir): update for onedir salt package
    [BREAKING CHANGE] update for onedir salt package

--- test/integration/share/libraries/libvirt_packages.rb
+++ test/integration/share/libraries/libvirt_packages.rb
@@ -4,7 +4,6 @@
 # Author: Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
 # Copyright (C) 2019 Pole de Competences Logiciels Libres <eole@ac-dijon.fr>
 
-# rubocop:disable Metrics/ClassLength
 class LibvirtPackagesResource < Inspec.resource(1)
   name 'libvirt_packages'
 
@@ -28,7 +27,6 @@ class LibvirtPackagesResource < Inspec.resource(1)
     packages = build_default_packages
 
     packages.merge!(build_os_family_packages)
-    packages.merge!(build_os_name_packages)
   end
 
   def build_os_family_packages
@@ -48,30 +46,11 @@ class LibvirtPackagesResource < Inspec.resource(1)
     end
   end
 
-  def build_os_name_packages
-    # osfamily.yaml / osmap.yaml
-    case inspec.system.platform[:name]
-    when 'centos', 'almalinux', 'rockylinux'
-      build_centos_packages
-
-    when 'ubuntu'
-      build_ubuntu_packages
-
-    else
-      {}
-    end
-  end
-
   def build_default_packages
     {
       'libvirt' => ['libvirt'],
       'qemu' => ['qemu-kvm'],
-      'extra' => ['libguestfs'],
-      'python' => if inspec.salt_minion.python3?
-                    ['libvirt-python3']
-                  else
-                    ['libvirt-python']
-                  end
+      'extra' => ['libguestfs']
     }
   end
 
@@ -79,67 +58,14 @@ class LibvirtPackagesResource < Inspec.resource(1)
     {
       'libvirt' => ['libvirt-daemon-system'],
       'qemu' => ['qemu-system-x86'],
-      'extra' => %w[libguestfs0 libguestfs-tools gnutls-bin],
-      'python' => if inspec.salt_minion.python3?
-                    ['python3-libvirt']
-                  else
-                    ['python-libvirt']
-                  end
-    }
-  end
-
-  def build_fedora_packages
-    {
-      'python' => if inspec.salt_minion.python3?
-                    ['python3-libvirt']
-                  else
-                    ['python2-libvirt']
-                  end
+      'extra' => %w[libguestfs0 libguestfs-tools gnutls-bin]
     }
   end
 
   def build_suse_packages
     {
       'libvirt' => ['libvirt-daemon-qemu'],
-      'extra' => ['libguestfs0'],
-      'python' => build_suse_python_package
+      'extra' => ['libguestfs0']
     }
   end
-
-  def build_suse_python_package
-    case inspec.system.platform[:release]
-    when 'tumbleweed'
-      ['python38-libvirt-python']
-    else
-      if inspec.salt_minion.python3?
-        ['python3-libvirt-python']
-      else
-        ['python2-libvirt-python']
-      end
-    end
-  end
-
-  def build_centos_packages
-    case inspec.system.platform[:release]
-    when /^7/
-      if inspec.salt_minion.python3?
-        { 'python' => [] }
-      else
-        { 'python' => ['libvirt-python'] }
-      end
-    else
-      # Only python3 since CentOS 8
-      { 'python' => ['python3-libvirt'] }
-    end
-  end
-
-  def build_ubuntu_packages
-    case inspec.system.platform[:release]
-    when /^16/
-      { 'libvirt' => ['libvirt-bin'] }
-    else
-      {}
-    end
-  end
 end
-# rubocop:enable Metrics/ClassLength
